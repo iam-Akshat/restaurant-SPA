94d1587cddbee267dc3e013e3288be973ac5237e
Akshat <masterakshata@gmail.com>
2021-03-15 19:06:11 +0530

    home section complete

---
+++ src/home.js
@@ -0,0 +1,16 @@
+import bgImg from "./assets/restua-bg.jpg" 
+const loadHome = () => {
+    const root = document.getElementById('content')
+    const home =  document.createElement('div')
+    home.classList.add('home')
+    home.style.background = `url(${bgImg})`
+    home.style.height = "100vh"
+    const homeWrapper = document.createElement('div')
+    home.appendChild(homeWrapper)
+    homeWrapper.innerHTML = `<h1>Serving the best burgers in town</h1>
+    <p>"Best burger I ever had"-<em>Donald Trump</em></p>`
+
+    root.appendChild(home)
+}
+
+export { loadHome }
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
-import bgImg from "./assets/restua-bg.jpg" 
+import { loadHome } from "./home"
 import maincss from "./stylesheets/styles.css"
+loadHome()
